71e7ed5cfdf0acc6c52783492874416cf6e8cdf2
JaeHeon Lee <romeo92s@naver.com>
2022-09-19 23:49:02 +0900

    "Test code"

--- src/main/java/com/example/junit_prac/entity/BookRepository.java
+++ src/main/java/com/example/junit_prac/entity/BookRepository.java
@@ -3,4 +3,5 @@ package com.example.junit_prac.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+
 }
--- src/test/java/com/example/junit_prac/entity/BookRepositoryTest.java
+++ src/test/java/com/example/junit_prac/entity/BookRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.example.junit_prac.entity;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,6 +15,11 @@ public class BookRepositoryTest {
 
     @Autowired //DI
     private BookRepository bookRepository;
+
+    @BeforeAll
+    public void 데이터준비(){
+
+    }
 
     //1.책등록
     @Test
@@ -29,11 +37,48 @@ public class BookRepositoryTest {
         //then(검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
-    }
+    } //트랜잭션 종료 (테스트가 종료되면서 저장된 데이터를 초기화함)
 
     //2.책 목록보기
+    @Test
+    public void 책목록보기_test(){
+        //given
+        String title = "JUnit5";
+        String author = "이재헌";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+        //when(테스트 실행)
+//        Book bookPS = bookRepository.save(book);
+        //when
+        List<Book> booksPS =bookRepository.findAll();
+        //then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+
+    }
 
     //3.책 한건보기
+    @Test
+    public void 책한건보기_test(){
+        //given
+        String title = "JUnit5";
+        String author = "이재헌";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+        //when
+
+        Book bookPS = bookRepository.findById(1L).get();
+
+        //then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    }
 
     //4.책 수정
 
